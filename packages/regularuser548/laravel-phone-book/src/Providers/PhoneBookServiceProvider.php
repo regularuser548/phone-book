84c114148d05f78dcdf5d7547492d78f9affd1c9
regularuser548 <regularuser548@tutanota.com>
2025-05-31 22:34:42 +0000

    Add initial Phone Book package scaffolding

---
+++ packages/regularuser548/laravel-phone-book/src/Providers/PhoneBookServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Regularuser548\PhoneBook\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class PhoneBookServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+
+    }
+}
